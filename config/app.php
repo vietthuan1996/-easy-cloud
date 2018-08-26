9608c3848f7ee79a7a846bbd8b67e544702d3214
Thuan <lanhvietthuan1996@gmail.com>
2018-08-26 19:57:25 +0700

    Create admin

--- config/app.php
+++ config/app.php
@@ -150,7 +150,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Intervention\Image\ImageServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -208,7 +207,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
